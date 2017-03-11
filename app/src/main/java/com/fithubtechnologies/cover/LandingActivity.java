1d04e2be1232a05bdc426dcf3650d9d306e03cd2
alxsnchez <alxsnchez@gmail.com>
2017-03-12 00:54:08 +0100

    Implemented OneSignal

--- app/src/main/java/com/fithubtechnologies/cover/LandingActivity.java
+++ app/src/main/java/com/fithubtechnologies/cover/LandingActivity.java
@@ -5,16 +5,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.onesignal.OneSignal;
+
 public class LandingActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+
+        OneSignal.startInit(this).init();
     }
 
     public void presentMainActivity(View view){
         Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void presentLoginActivity(View view){
+        Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
 }
